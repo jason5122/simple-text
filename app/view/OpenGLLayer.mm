4983f7bf01d631acf05d4768b4e5ffc46a2153bf
Jason Han <jjhan@alum.vassar.edu>
2024-01-07 09:56:37 -0500

    Begin porting Alacritty renderer.rs

--- app/view/OpenGLLayer.mm
+++ app/view/OpenGLLayer.mm
@@ -6,10 +6,38 @@
 #import "util/LogUtil.h"
 #import <OpenGL/gl3.h>
 
+struct SizeInfo {
+    float width;
+    float height;
+    float cell_width;
+    float cell_height;
+    float padding_x;
+    float padding_y;
+};
+
+struct InstanceData {
+    uint16_t col;
+    uint16_t row;
+
+    int16_t left;
+    int16_t top;
+    int16_t width;
+    int16_t height;
+
+    float uv_left;
+    float uv_bot;
+    float uv_width;
+    float uv_height;
+};
+
 @interface OpenGLLayer () {
     GLuint shaderProgram;
-    GLuint vao, vbo;
+    GLuint vao;
+    GLuint ebo;
+    GLuint vbo;
     GLfloat screenWidth, screenHeight;
+    GLint u_cell_dim;
+    SizeInfo size_info;
 }
 @end
 
@@ -56,15 +84,39 @@
         logDefault(@"OpenGL", @"average_advance = %f, line_height = %f", metrics.average_advance,
                    metrics.line_height);
 
+        float cell_width = floor(metrics.average_advance + 1);
+        float cell_height = floor(metrics.line_height + 2);
+        logDefault(@"OpenGL", @"cell_width = %f, cell_height = %f", cell_width, cell_height);
+
         Rasterizer rasterizer = Rasterizer();
-        CGGlyph glyph = menlo.get_glyph(@"E");
-        RasterizedGlyph rasterized_glyph = rasterizer.rasterize_glyph(glyph);
+        CGGlyph glyph_index = menlo.get_glyph(@"E");
+        RasterizedGlyph rasterized_glyph = rasterizer.rasterize_glyph(glyph_index);
 
         Atlas atlas = Atlas(1024);
-        atlas.insert_inner(rasterized_glyph);
+        Glyph glyph = atlas.insert_inner(rasterized_glyph);
 
         logDefault(@"OpenGL", @"%s", glGetString(GL_VERSION));
         logDefault(@"OpenGL", @"%fx%f", screenWidth, screenHeight);
+
+        logDefault(@"OpenGL", @"viewport_size: %fx%f", self.frame.size.width,
+                   self.frame.size.height);
+        float viewport_width = self.frame.size.width;
+        float viewport_height = self.frame.size.height;
+        size_info = {viewport_width * 2, viewport_height * 2, cell_width, cell_height, 10.0, 10.0};
+
+        glViewport(size_info.padding_x, size_info.padding_y,
+                   size_info.width - 2 * size_info.padding_x,
+                   size_info.height - 2 * size_info.padding_y);
+        glUseProgram(shaderProgram);
+
+        GLuint uProjection = glGetUniformLocation(shaderProgram, "projection");
+        float scale_x = 2.0 / (size_info.width - 2.0 * size_info.padding_x);
+        float scale_y = -2.0 / (size_info.height - 2.0 * size_info.padding_y);
+        float offset_x = -1.0;
+        float offset_y = 1.0;
+        logDefault(@"OpenGL", @"%f %f", scale_x, scale_y);
+        glUniform4f(uProjection, offset_x, offset_y, scale_x, scale_y);
+        glUseProgram(0);
 
         GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
         GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -88,24 +140,21 @@
     return context;
 }
 
+- (void)draw {
+    glUseProgram(shaderProgram);
+    glUniform2f(u_cell_dim, size_info.cell_width, size_info.cell_height);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glActiveTexture(GL_TEXTURE0);
+}
+
 - (BOOL)canDrawInCGLContext:(CGLContextObj)glContext
                 pixelFormat:(CGLPixelFormatObj)pixelFormat
                forLayerTime:(CFTimeInterval)timeInterval
                 displayTime:(const CVTimeStamp*)timeStamp {
     return true;
-}
-
-- (void)draw {
-    glViewport(0, 0, screenWidth, screenHeight);
-
-    GLuint uResolution = glGetUniformLocation(shaderProgram, "resolution");
-    glUniform2fv(uResolution, 1, new GLfloat[]{screenWidth, screenHeight});
-
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glUseProgram(shaderProgram);
-    glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 18);
 }
 
 - (void)drawInCGLContext:(CGLContextObj)glContext

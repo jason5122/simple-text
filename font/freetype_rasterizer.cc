6cd51589a4577ebc42cbda88123a2abaefb5d876
Jason Han <jjhan@alum.vassar.edu>
2024-02-10 07:27:43 -0500

    Add //third_party/harfbuzz

--- font/freetype_rasterizer.cc
+++ font/freetype_rasterizer.cc
@@ -1,4 +1,5 @@
 #include "freetype_rasterizer.h"
+#include <hb.h>
 #include <iostream>
 #include <vector>
 
@@ -33,6 +34,49 @@ bool FreeTypeRasterizer::setup(const char* font_path) {
 
     this->line_height = std::max(glyph_height, global_glyph_height) + 2;
     this->descent = descent;
+
+    hb_buffer_t* buf;
+    buf = hb_buffer_create();
+    hb_buffer_add_utf8(buf, "Hello world!", -1, 0, -1);
+
+    // If you know the direction, script, and language
+    hb_buffer_set_direction(buf, HB_DIRECTION_LTR);
+    hb_buffer_set_script(buf, HB_SCRIPT_LATIN);
+    hb_buffer_set_language(buf, hb_language_from_string("en", -1));
+
+    // If you don't know the direction, script, and language
+    // hb_buffer_guess_segment_properties(buffer);
+
+    hb_blob_t* blob =
+        hb_blob_create_from_file(font_path); /* or hb_blob_create_from_file_or_fail() */
+    hb_face_t* face = hb_face_create(blob, 0);
+    hb_font_t* font = hb_font_create(face);
+
+    hb_shape(font, buf, NULL, 0);
+
+    unsigned int glyph_count;
+    hb_glyph_info_t* glyph_info = hb_buffer_get_glyph_infos(buf, &glyph_count);
+    hb_glyph_position_t* glyph_pos = hb_buffer_get_glyph_positions(buf, &glyph_count);
+
+    hb_position_t cursor_x = 0;
+    hb_position_t cursor_y = 0;
+    for (unsigned int i = 0; i < glyph_count; i++) {
+        hb_codepoint_t glyphid = glyph_info[i].codepoint;
+        hb_position_t x_offset = glyph_pos[i].x_offset;
+        hb_position_t y_offset = glyph_pos[i].y_offset;
+        hb_position_t x_advance = glyph_pos[i].x_advance;
+        hb_position_t y_advance = glyph_pos[i].y_advance;
+        /* draw_glyph(glyphid, cursor_x + x_offset, cursor_y + y_offset); */
+        cursor_x += x_advance;
+        cursor_y += y_advance;
+
+        std::cerr << glyphid << '\n';
+    }
+
+    hb_buffer_destroy(buf);
+    hb_font_destroy(font);
+    hb_face_destroy(face);
+    hb_blob_destroy(blob);
 
     return true;
 }

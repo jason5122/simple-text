90472122871615224180ff5da78ea15b80103c45
Jason Han <jjhan@alum.vassar.edu>
2024-02-12 14:23:02 -0500

    Use enum utils for fredbuf

--- base/tests/fredbuf_test.cc
+++ base/tests/fredbuf_test.cc
@@ -14,19 +14,11 @@ TEST(FredbufTest, Basic) {
 
     tree.insert(CharOffset{0}, "foo");
 
-    LineRange line_range = tree.get_line_range(Line{2});
-    size_t line_start_offset =
-        static_cast<typename std::underlying_type<CharOffset>::type>(line_range.first);
-    std::cout << line_start_offset << '\n';
+    CharOffset line_offset = tree.get_line_range(Line{2}).first;
+    tree.remove(line_offset + Length{5}, Length{1});
+    tree.insert(line_offset + Length{5}, "B");
 
-    tree.remove(CharOffset{line_start_offset + 5}, Length{1});
-    tree.insert(CharOffset{line_start_offset + 5}, "B");
-
-    std::cout << text << '\n';
-
-    size_t line_count =
-        static_cast<typename std::underlying_type<CharOffset>::type>(tree.line_count());
-    for (size_t line_index = 1; line_index < line_count; line_index++) {
+    for (size_t line_index = 1; line_index < rep(tree.line_count()); line_index++) {
         std::string buf;
         tree.get_line_content(&buf, Line{line_index});
         std::cout << buf << '\n';

ba630faebc56a5fd4e6b81a30818f992d5868f78
Jason Han <jjhan@alum.vassar.edu>
2024-02-13 19:41:14 -0500

    Implement rounded corners proof of concept!

--- ui/renderer/shaders/rect_frag.glsl
+++ ui/renderer/shaders/rect_frag.glsl
@@ -4,36 +4,22 @@ flat in vec4 color;
 
 out vec4 frag_color;
 
-flat in vec2 upper_left;
-in vec2 size;
+flat in vec2 center;
+flat in vec2 size;
 
 uniform vec2 resolution;
 
 void main() {
+    float corner_radius = 200;
+
     vec2 loc = gl_FragCoord.xy;
 
-    // if (loc.x < 0.5 && loc.y < 0.5) {
-    //     discard;
-    // }
-
-    // if (loc.x < 0.5 && loc.y < upper_left.y) {
-    //     discard;
-    // }
-
-    // vec2 upper_left_scaled = upper_left / resolution;
-    // if (upper_left_scaled.y < 0.5) {
-    //     discard;
-    // }
-
-    float x = upper_left.x;
-    float y = upper_left.y;
-    // if (distance(loc.x, x) < 0.1 && distance(loc.y, y) < 0.1) {
-    //     discard;
-    // }
-
-    vec2 r0 = vec2(x, y);
-    if (distance(loc, r0) < 200) {
-        discard;
+    vec2 top_right = center + size / 2;
+    top_right -= corner_radius;
+    if (loc.x > top_right.x && loc.y > top_right.y) {
+        if (distance(loc, top_right) > corner_radius) {
+            discard;
+        }
     }
 
     frag_color = color;
--- ui/renderer/shaders/rect_vert.glsl
+++ ui/renderer/shaders/rect_vert.glsl
@@ -6,8 +6,8 @@ layout(location = 2) in vec4 in_color;
 
 flat out vec4 color;
 
-flat out vec2 upper_left;
-out vec2 size;
+flat out vec2 center;
+flat out vec2 size;
 
 uniform vec2 resolution;
 uniform vec2 scroll_offset;
@@ -31,6 +31,6 @@ void main() {
     gl_Position = vec4(pixelToClipSpace(final_position), 0.0, 1.0);
     color = in_color / 255.0;
 
-    upper_left = final_position + rect_size / 2;
-    size = vec2(0, 0);
+    center = final_position + rect_size / 2;
+    size = rect_size;
 }

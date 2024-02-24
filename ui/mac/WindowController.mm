895c5361fd1b75ee5c0c0d443b0ca2b16c4a7f80
Jason Han <jjhan@alum.vassar.edu>
2024-02-24 10:38:42 -0500

    Remove //third_party/stb_image; remove EditorViewNew

--- ui/mac/WindowController.mm
+++ ui/mac/WindowController.mm
@@ -19,12 +19,9 @@
         //                                                   alpha:1.f];
 
         editorView = [[EditorView alloc] initWithFrame:frameRect];
-        editorViewNew = [[EditorViewNew alloc] initWithFrame:frameRect];
         self.window.contentView = editorView;
-        // self.window.contentView = editorViewNew;
 
         [self.window makeFirstResponder:editorView];
-        // [self.window makeFirstResponder:editorViewNew];
     }
     return self;
 }

f82846bac2be351d965e0a5af0f34680c6d7e9f4
Jason Han <jjhan@alum.vassar.edu>
2024-03-04 18:21:56 -0500

    Use GTK 3 instead of GTK 4

--- ui/gtk/editor_window.cc
+++ ui/gtk/editor_window.cc
@@ -4,19 +4,28 @@ static void print_hello(GtkWidget* widget, gpointer data) {
     g_print("Hello World\n");
 }
 
+// static void activate(GtkApplication* app, gpointer user_data) {
+//     GtkWidget* window;
+//     GtkWidget* button;
+
+//     window = gtk_application_window_new(app);
+//     gtk_window_set_title(GTK_WINDOW(window), "Hello");
+//     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+
+//     button = gtk_button_new_with_label("Hello World");
+//     g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+//     gtk_window_set_child(GTK_WINDOW(window), button);
+
+//     gtk_window_present(GTK_WINDOW(window));
+// }
+
 static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget* window;
-    GtkWidget* button;
 
     window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Hello");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-
-    button = gtk_button_new_with_label("Hello World");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-    gtk_window_set_child(GTK_WINDOW(window), button);
-
-    gtk_window_present(GTK_WINDOW(window));
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 400);
+    gtk_widget_show_all(window);
 }
 
 EditorWindow::EditorWindow() {

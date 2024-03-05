237048915261830024cfeb441e4aaf669c89286c
Jason Han <jjhan@alum.vassar.edu>
2024-03-05 16:13:02 -0500

    Linux: Use libepoxy instead of GLAD

--- ui/gtk/editor_window.cc
+++ ui/gtk/editor_window.cc
@@ -1,22 +1,49 @@
 #include "editor_window.h"
+#include <epoxy/gl.h>
 
 static gboolean my_keypress_function(GtkWidget* widget, GdkEventKey* event, gpointer data) {
     if (event->keyval == GDK_KEY_q && event->state & GDK_META_MASK) {
         g_application_quit(G_APPLICATION(data));
         return true;
     }
+    if (event->keyval == GDK_KEY_q && event->state & GDK_CONTROL_MASK) {
+        g_application_quit(G_APPLICATION(data));
+        return true;
+    }
     return false;
 }
 
-static void activate(GtkApplication* app, gpointer user_data) {
-    GtkWidget* window;
+static gboolean render(GtkGLArea* area, GdkGLContext* context) {
+    // inside this function it's safe to use GL; the given
+    // `GdkGLContext` has been made current to the drawable
+    // surface used by the `GtkGLArea` and the viewport has
+    // already been set to be the size of the allocation
 
-    window = gtk_application_window_new(app);
+    glClearColor(1, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // we completed our drawing; the draw commands will be
+    // flushed at the end of the signal emission chain, and
+    // the buffers will be drawn on the window
+    return true;
+}
+
+static void activate(GtkApplication* app, gpointer user_data) {
+    GtkWidget* window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 400);
-    gtk_widget_show_all(window);
     gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
     g_signal_connect(G_OBJECT(window), "key_press_event", G_CALLBACK(my_keypress_function), app);
+
+    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, false);
+    gtk_box_set_spacing(GTK_BOX(box), 6);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    GtkWidget* gl_area = gtk_gl_area_new();
+    gtk_box_pack_start(GTK_BOX(box), gl_area, 1, 1, 0);
+    g_signal_connect(gl_area, "render", G_CALLBACK(render), nullptr);
+
+    gtk_widget_show_all(window);
 }
 
 EditorWindow::EditorWindow() {

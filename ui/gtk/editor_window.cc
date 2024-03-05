1d2574a3703421327df626504cebe15ab979637a
Jason Han <jjhan@alum.vassar.edu>
2024-03-04 22:02:30 -0500

    GTK: Check GLib version

--- ui/gtk/editor_window.cc
+++ ui/gtk/editor_window.cc
@@ -1,24 +1,5 @@
 #include "editor_window.h"
 
-static void print_hello(GtkWidget* widget, gpointer data) {
-    g_print("Hello World\n");
-}
-
-// static void activate(GtkApplication* app, gpointer user_data) {
-//     GtkWidget* window;
-//     GtkWidget* button;
-
-//     window = gtk_application_window_new(app);
-//     gtk_window_set_title(GTK_WINDOW(window), "Hello");
-//     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-
-//     button = gtk_button_new_with_label("Hello World");
-//     g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-//     gtk_window_set_child(GTK_WINDOW(window), button);
-
-//     gtk_window_present(GTK_WINDOW(window));
-// }
-
 static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget* window;
 
@@ -29,7 +10,12 @@ static void activate(GtkApplication* app, gpointer user_data) {
 }
 
 EditorWindow::EditorWindow() {
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+#if GLIB_CHECK_VERSION(2, 74, 0)
+    GApplicationFlags flags = G_APPLICATION_DEFAULT_FLAGS;
+#else
+    GApplicationFlags flags = G_APPLICATION_FLAGS_NONE;
+#endif
+    app = gtk_application_new("org.gtk.example", flags);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 }
 

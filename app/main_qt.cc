4f513991d2d3e271f3f8e95f0f18cf7b9974b117
Jason Han <jjhan@alum.vassar.edu>
2024-03-05 20:41:05 -0500

    Remove //ui/qt, //ui/linux, and //third_party/glad

--- app/main_qt.cc
+++
@@ -1,6 +0,0 @@
-#include "ui/qt/editor_window_qt.h"
-
-int SimpleTextMain(int argc, char* argv[]) {
-    EditorWindowQt window(argc, argv);
-    return window.run();
-}
--- ui/linux/wayland_client.cc
+++
@@ -1,6 +0,0 @@
-#include "wayland_client.h"
-
-bool WaylandClient::connectToDisplay() {
-    display = wl_display_connect(NULL);
-    return display;
-}

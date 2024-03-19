5b7dd4f86877301308536d389339fede6fcbd036
Jason Han <jjhan@alum.vassar.edu>
2024-03-19 12:49:18 -0400

    Use .at() instead of [] for vector access

--- base/buffer.cc
+++ base/buffer.cc
@@ -28,13 +28,13 @@ size_t Buffer::lineCount() {
 }
 
 void Buffer::getLineContent(std::string* buf, size_t line_index) const {
-    *buf = data[line_index];
+    *buf = data.at(line_index);
 }
 
 size_t Buffer::byteOfLine(size_t line_index) {
     size_t byte_offset = 0;
     for (uint16_t row = 0; row < line_index; row++) {
-        byte_offset += data[row].size();
+        byte_offset += data.at(row).size();
         byte_offset++;  // Include newline.
     }
     return byte_offset;
@@ -42,34 +42,34 @@ size_t Buffer::byteOfLine(size_t line_index) {
 
 void Buffer::insert(size_t line_index, size_t line_offset, std::string_view txt) {
     if (txt == "\n") {
-        std::string before_lf = data[line_index].substr(0, line_offset);
-        std::string after_lf = data[line_index].substr(line_offset);
+        std::string before_lf = data.at(line_index).substr(0, line_offset);
+        std::string after_lf = data.at(line_index).substr(line_offset);
 
         data.insert(data.begin() + line_index + 1, after_lf);
-        data[line_index] = before_lf;
+        data.at(line_index) = before_lf;
     } else {
-        data[line_index].insert(line_offset, txt);
+        data.at(line_index).insert(line_offset, txt);
     }
 }
 
 void Buffer::remove(size_t line_index, size_t line_offset, size_t bytes) {
-    if (line_offset == data[line_index].length()) {
+    if (line_offset == data.at(line_index).length()) {
         if (line_index < lineCount() - 1) {
-            data[line_index] += data[line_index + 1];
+            data.at(line_index) += data.at(line_index + 1);
             data.erase(data.begin() + line_index + 1);
         }
     } else {
-        data[line_index].erase(line_offset, bytes);
+        data.at(line_index).erase(line_offset, bytes);
     }
 }
 
 void Buffer::backspace(size_t line_index, size_t line_offset, size_t bytes) {
     if (line_offset == 0) {
         if (line_index > 0) {
-            data[line_index - 1] += data[line_index];
+            data.at(line_index - 1) += data.at(line_index);
             data.erase(data.begin() + line_index);
         }
     } else {
-        data[line_index].erase(line_offset - bytes, bytes);
+        data.at(line_index).erase(line_offset - bytes, bytes);
     }
 }

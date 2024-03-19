9961a69d869db3a0d168e168a18d7a5af51e9e0e
Jason Han <jjhan@alum.vassar.edu>
2024-03-19 17:35:17 -0400

    Fix glyph advance not being rounded

--- ui/renderer/shaders/text_frag.glsl
+++ ui/renderer/shaders/text_frag.glsl
@@ -25,94 +25,111 @@ void main() {
         vec3 temp = bg_color.rgb;
         float tab_corner_radius = 6;
 
-        // if (tab_corner_radius > 0) {
-        //     vec2 center = gl_FragCoord.xy - bg_center;
-        //     float d = roundedBoxSDF(center, bg_size / 2, tab_corner_radius);
-        //     alpha -= smoothstep(-0.5, 0.5, d);
-        // }
-
         if (tab_corner_radius > 0) {
-            vec2 pixel_pos = gl_FragCoord.xy;
-
-            vec2 bottom_left = bg_center - bg_size / 2;
-            vec2 bottom_right = bg_center + vec2(bg_size.x / 2, -bg_size.y / 2);
-            vec2 top_left = bg_center + vec2(-bg_size.x / 2, bg_size.y / 2);
-            vec2 top_right = bg_center + bg_size / 2;
-
-            bottom_left += tab_corner_radius;
-            bottom_right += vec2(-tab_corner_radius, tab_corner_radius);
-            top_left += vec2(tab_corner_radius, -tab_corner_radius);
-            top_right += vec2(-tab_corner_radius, -tab_corner_radius);
-
-            // if (pixel_pos.x < bottom_left.x) {
-            //     if (pixel_pos.y > bottom_left.y) {
-            //         // discard;
-            //     } else {
-            //         vec2 point = bg_center - bg_size / 2;
-            //         point += vec2(0, tab_corner_radius);
-
-            //         float d = distance(pixel_pos, point) - tab_corner_radius;
-            //         alpha -= smoothstep(-0.5, 0.5, 1.0 - d);
-            //     }
-            // }
-            // if (pixel_pos.x > bottom_right.x) {
-            //     if (pixel_pos.y > bottom_right.y) {
-            //         // discard;
-            //     } else {
-            //         vec2 point = bg_center + vec2(bg_size.x / 2, -bg_size.y / 2);
-            //         // point += vec2(0, tab_corner_radius);
-
-            //         float d = distance(pixel_pos, point) - tab_corner_radius;
-            //         // alpha -= smoothstep(-0.5, 0.5, 1.0 - d);
-
-            //         if (d > 0) {
-            //             alpha = 0;
-            //         }
-
-            //         // if (d < -1 || d >= 0) {
-            //         //     alpha = 0;
-            //         // }
-            //     }
-            // }
-            if (pixel_pos.x < top_left.x && pixel_pos.y > top_left.y) {
-                float d = distance(pixel_pos, top_left) - tab_corner_radius;
-                // alpha -= smoothstep(-0.5, 0.5, d);
-
-                if (-2 < d && d < 0) {
-                    temp = vec3(1.0, 0.0, 0.0);
-                } else if (d > 0) {
-                    alpha = 0;
-                }
+            vec2 center = gl_FragCoord.xy - bg_center;
+            float d = roundedBoxSDF(center, bg_size / 2, tab_corner_radius);
+            if (d < 0) {
+                alpha = 1.0;
+            } else {
+                alpha = 0.0;
             }
-            if (pixel_pos.x > top_right.x && pixel_pos.y > top_right.y) {
-                float d = distance(pixel_pos, top_right) - tab_corner_radius;
-                // alpha -= smoothstep(-0.5, 0.5, d);
-
-                if (-2 < d && d < 0) {
-                    temp = vec3(1.0, 0.0, 0.0);
-                } else if (d > 0) {
-                    alpha = 0;
-                }
-            }
-
-            // float border_thickness = 2;
-            // float border_left = bg_center.x - bg_size.x / 2 + border_thickness;
-            // float border_right = bg_center.x + bg_size.x / 2 - border_thickness;
-            // float border_top = bg_center.y + bg_size.y / 2 - border_thickness;
-            // float border_bottom = bg_center.y - bg_size.y / 2 + border_thickness;
-            // if (pixel_pos.x < border_left) {
-            //     temp = vec3(1.0, 0.0, 0.0);
-            // }
-            // if (pixel_pos.x > border_right) {
-            //     temp = vec3(1.0, 0.0, 0.0);
-            // }
-            // if (pixel_pos.y > border_top) {
-            //     temp = vec3(1.0, 0.0, 0.0);
-            // }
-            // if (pixel_pos.y < border_bottom) {
-            //     temp = vec3(1.0, 0.0, 0.0);
-            // }
         }
+
+        // if (tab_corner_radius > 0) {
+        //     vec2 pixel_pos = gl_FragCoord.xy;
+
+        //     vec2 bottom_left = bg_center - bg_size / 2;
+        //     vec2 bottom_right = bg_center + vec2(bg_size.x / 2, -bg_size.y / 2);
+        //     vec2 top_left = bg_center + vec2(-bg_size.x / 2, bg_size.y / 2);
+        //     vec2 top_right = bg_center + bg_size / 2;
+
+        //     bottom_left += tab_corner_radius;
+        //     bottom_right += vec2(-tab_corner_radius, tab_corner_radius);
+        //     top_left += vec2(tab_corner_radius, -tab_corner_radius);
+        //     top_right += vec2(-tab_corner_radius, -tab_corner_radius);
+
+        //     // if (pixel_pos.x < bottom_left.x) {
+        //     //     if (pixel_pos.y > bottom_left.y) {
+        //     //         // discard;
+        //     //     } else {
+        //     //         vec2 point = bg_center - bg_size / 2;
+        //     //         point += vec2(0, tab_corner_radius);
+
+        //     //         float d = distance(pixel_pos, point) - tab_corner_radius;
+        //     //         alpha -= smoothstep(-0.5, 0.5, 1.0 - d);
+        //     //     }
+        //     // }
+        //     // if (pixel_pos.x > bottom_right.x) {
+        //     //     if (pixel_pos.y > bottom_right.y) {
+        //     //         // discard;
+        //     //     } else {
+        //     //         vec2 point = bg_center + vec2(bg_size.x / 2, -bg_size.y / 2);
+        //     //         // point += vec2(0, tab_corner_radius);
+
+        //     //         float d = distance(pixel_pos, point) - tab_corner_radius;
+        //     //         // alpha -= smoothstep(-0.5, 0.5, 1.0 - d);
+
+        //     //         if (d > 0) {
+        //     //             alpha = 0;
+        //     //         }
+
+        //     //         // if (d < -1 || d >= 0) {
+        //     //         //     alpha = 0;
+        //     //         // }
+        //     //     }
+        //     // }
+        //     if (pixel_pos.x < bottom_left.x && pixel_pos.y > bottom_left.y) {
+        //         float d = distance(pixel_pos, bottom_left) - tab_corner_radius;
+        //         // alpha -= smoothstep(-0.5, 0.5, d);
+
+        //         if (-2 < d && d < 0) {
+        //             temp = vec3(1.0, 0.0, 0.0);
+        //         } else if (d > 0) {
+        //             // FIXME: This doesn't blend into the background color.
+        //             alpha = 0;
+        //         }
+        //     }
+        //     if (pixel_pos.x < top_left.x && pixel_pos.y > top_left.y) {
+        //         float d = distance(pixel_pos, top_left) - tab_corner_radius;
+        //         // alpha -= smoothstep(-0.5, 0.5, d);
+
+        //         if (-2 < d && d < 0) {
+        //             temp = vec3(1.0, 0.0, 0.0);
+        //         } else if (d > 0) {
+        //             // FIXME: This doesn't blend into the background color.
+        //             alpha = 0;
+        //         }
+        //     }
+        //     if (pixel_pos.x > top_right.x && pixel_pos.y > top_right.y) {
+        //         float d = distance(pixel_pos, top_right) - tab_corner_radius;
+        //         // alpha -= smoothstep(-0.5, 0.5, d);
+
+        //         if (-2 < d && d < 0) {
+        //             temp = vec3(1.0, 0.0, 0.0);
+        //         } else if (d > 0) {
+        //             // FIXME: This doesn't blend into the background color.
+        //             alpha = 0;
+        //         }
+        //     }
+
+        //     // float border_thickness = 2;
+        //     // float border_left = bg_center.x - bg_size.x / 2 + border_thickness;
+        //     // float border_right = bg_center.x + bg_size.x / 2 - border_thickness;
+        //     // float border_top = bg_center.y + bg_size.y / 2 - border_thickness;
+        //     // float border_bottom = bg_center.y - bg_size.y / 2 + border_thickness;
+        //     // if (pixel_pos.x < border_left) {
+        //     //     temp = vec3(1.0, 0.0, 0.0);
+        //     // }
+        //     // if (pixel_pos.x > border_right) {
+        //     //     temp = vec3(1.0, 0.0, 0.0);
+        //     // }
+        //     // if (pixel_pos.y > border_top) {
+        //     //     temp = vec3(1.0, 0.0, 0.0);
+        //     // }
+        //     // if (pixel_pos.y < border_bottom) {
+        //     //     temp = vec3(1.0, 0.0, 0.0);
+        //     // }
+        // }
 
         alpha_mask = vec4(1.0);
         // color = vec4(bg_color.rgb * bg_color.a, bg_color.a);

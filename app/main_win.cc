0ae0ad8960fc0d297fc691244823a8c03ed335ba
Jason Han <jjhan@alum.vassar.edu>
2024-03-29 14:00:56 -0400

    Win: Remove non-base window implementation

--- app/main_win.cc
+++ app/main_win.cc
@@ -1,34 +1,9 @@
-#include "ui/win32/editor_window.h"
+#include "ui/win32/main_window.h"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-    // Register the window class.
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
+    MainWindow win;
 
-    WNDCLASS wc = {};
-
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-
-    RegisterClass(&wc);
-
-    // Create the window.
-
-    HWND hwnd = CreateWindowEx(0,                            // Optional window styles.
-                               CLASS_NAME,                   // Window class
-                               L"Learn to Program Windows",  // Window text
-                               WS_OVERLAPPEDWINDOW,          // Window style
-
-                               // Size and position
-                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-                               NULL,       // Parent window
-                               NULL,       // Menu
-                               hInstance,  // Instance handle
-                               NULL        // Additional application data
-    );
-
-    if (hwnd == NULL) {
+    if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW)) {
         return 0;
     }
 
@@ -43,15 +18,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     placement.length = sizeof(WINDOWPLACEMENT);
     placement.showCmd = SW_SHOWMAXIMIZED;
     placement.rcNormalPosition = RECT{0, 0, 1000, 500};
-    SetWindowPlacement(hwnd, &placement);
-
-    // FIXME: This is visually glitchy.
-    // ShowWindow(hwnd, SW_SHOWMAXIMIZED);
-
-    // Run the message loop.
+    SetWindowPlacement(win.Window(), &placement);
 
     MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+    while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }

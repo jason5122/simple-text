2fa78624217b9390cd0a41edbc457f7934bea211
Jason Han <jjhan@alum.vassar.edu>
2024-03-31 09:22:40 -0400

    Win: Compile libepoxy (broken)

--- ui/win32/main_window.cc
+++ ui/win32/main_window.cc
@@ -3,12 +3,55 @@
 #include <iostream>
 #include <wingdi.h>
 
+BOOL bSetupPixelFormat(HDC hdc) {
+    PIXELFORMATDESCRIPTOR pfd, *ppfd;
+    int pixelformat;
+
+    ppfd = &pfd;
+
+    ppfd->nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    ppfd->nVersion = 1;
+    ppfd->dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    ppfd->dwLayerMask = PFD_MAIN_PLANE;
+    ppfd->iPixelType = PFD_TYPE_COLORINDEX;
+    ppfd->cColorBits = 8;
+    ppfd->cDepthBits = 16;
+    ppfd->cAccumBits = 0;
+    ppfd->cStencilBits = 0;
+
+    pixelformat = ChoosePixelFormat(hdc, ppfd);
+
+    if ((pixelformat = ChoosePixelFormat(hdc, ppfd)) == 0) {
+        // MessageBox(NULL, "ChoosePixelFormat failed", "Error", MB_OK);
+        return FALSE;
+    }
+
+    if (SetPixelFormat(hdc, pixelformat, ppfd) == FALSE) {
+        // MessageBox(NULL, "SetPixelFormat failed", "Error", MB_OK);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
 PCWSTR MainWindow::ClassName() const {
     return L"Sample Window Class";
 }
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
+    case WM_CREATE:
+        std::cerr << "WM_CREATE\n";
+        ghDC = GetDC(m_hwnd);
+        if (!bSetupPixelFormat(ghDC)) PostQuitMessage(0);
+
+        // TODO: Fix "Attempting to dlopen() while in the dynamic linker" error.
+        // ghRC = wglCreateContext(ghDC);
+        // wglMakeCurrent(ghDC, ghRC);
+        // epoxy_handle_external_wglMakeCurrent();
+        std::cerr << epoxy_gl_version() << '\n';
+        return 0;
+
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
--- ui/win32/main_window.h
+++ ui/win32/main_window.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include "ui/win32/base_window.h"
+#include <epoxy/gl.h>
+#include <epoxy/wgl.h>
 
 class MainWindow : public BaseWindow<MainWindow> {
 public:
     PCWSTR ClassName() const;
     LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+    HDC ghDC;
+    HGLRC ghRC;
 };

42c8078878a96c412235dc99ff32fcaf56598011
Jason Han <jjhan@alum.vassar.edu>
2024-04-04 21:10:57 -0400

    Linux: Implement App::createNewWindow() for OpenGL

--- ui/app/app_linux.cc
+++ ui/app/app_linux.cc
@@ -1,15 +1,13 @@
 #include "app.h"
+#include <glad/glad.h>
 #include <gtk/gtk.h>
 
 class App::impl {
 public:
     GtkApplication* app;
-
-    static void activate(GtkApplication* gtk_app, gpointer p_app);
-    static gboolean my_keypress_function(GtkWidget* widget, GdkEventKey* event, gpointer data);
 };
 
-gboolean App::impl::my_keypress_function(GtkWidget* widget, GdkEventKey* event, gpointer data) {
+static gboolean my_keypress_function(GtkWidget* widget, GdkEventKey* event, gpointer data) {
     if (event->keyval == GDK_KEY_w && event->state & GDK_CONTROL_MASK) {
         gtk_window_close(GTK_WINDOW(widget));
         return true;
@@ -21,9 +19,36 @@ gboolean App::impl::my_keypress_function(GtkWidget* widget, GdkEventKey* event, 
     return false;
 }
 
-void App::impl::activate(GtkApplication* gtk_app, gpointer p_app) {
+static void activate(GtkApplication* gtk_app, gpointer p_app) {
     App* app = static_cast<App*>(p_app);
     app->onActivate();
+}
+
+static gboolean render(GtkWidget* widget) {
+    // inside this function it's safe to use GL; the given
+    // `GdkGLContext` has been made current to the drawable
+    // surface used by the `GtkGLArea` and the viewport has
+    // already been set to be the size of the allocation
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // we completed our drawing; the draw commands will be
+    // flushed at the end of the signal emission chain, and
+    // the buffers will be drawn on the window
+    return true;
+}
+
+static void realize(GtkWidget* widget) {
+    gtk_gl_area_make_current(GTK_GL_AREA(widget));
+    if (gtk_gl_area_get_error(GTK_GL_AREA(widget)) != nullptr) return;
+
+    if (!gladLoadGL()) {
+        std::cerr << "Failed to initialize GLAD\n";
+    }
+
+    glEnable(GL_BLEND);
+    glDepthMask(GL_FALSE);
+
+    glClearColor(1.0, 0.0, 0.0, 1.0);
 }
 
 App::App() : pimpl{new impl{}} {
@@ -33,7 +58,7 @@ App::App() : pimpl{new impl{}} {
     GApplicationFlags flags = G_APPLICATION_FLAGS_NONE;
 #endif
     pimpl->app = gtk_application_new("com.jason.simple-text", flags);
-    g_signal_connect(pimpl->app, "activate", G_CALLBACK(pimpl->activate), this);
+    g_signal_connect(pimpl->app, "activate", G_CALLBACK(activate), this);
 }
 
 void App::run() {
@@ -44,11 +69,20 @@ void App::createNewWindow() {
     GtkWidget* window = gtk_application_window_new(pimpl->app);
     gtk_window_set_title(GTK_WINDOW(window), "Simple Text");
 
+    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, false);
+    gtk_box_set_spacing(GTK_BOX(box), 6);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    GtkWidget* gl_area = gtk_gl_area_new();
+    gtk_box_pack_start(GTK_BOX(box), gl_area, 1, 1, 0);
+    g_signal_connect(gl_area, "render", G_CALLBACK(render), nullptr);
+    g_signal_connect(gl_area, "realize", G_CALLBACK(realize), nullptr);
+
     gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
-    g_signal_connect(G_OBJECT(window), "key_press_event", G_CALLBACK(pimpl->my_keypress_function),
+    g_signal_connect(G_OBJECT(window), "key_press_event", G_CALLBACK(my_keypress_function),
                      pimpl->app);
 
-    gtk_window_maximize(GTK_WINDOW(window));
+    // gtk_window_maximize(GTK_WINDOW(window));
     // TODO: Set default window size without magic numbers.
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 400);
 

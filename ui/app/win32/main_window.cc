302c841029add29597cc444dfa4ccde21b310f65
Jason Han <jjhan@alum.vassar.edu>
2024-04-17 01:43:53 -0400

    Win: Use Blink's scrolling implementation

--- ui/app/win32/main_window.cc
+++ ui/app/win32/main_window.cc
@@ -106,15 +106,28 @@ LRESULT MainWindow::handleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
         // Invert vertical scrolling.
         dy *= -1;
 
-        // TODO: Replace this magic number.
-        dy *= 60;
+        static const unsigned long kDefaultScrollLinesPerWheelDelta = 3;
+        unsigned long scroll_lines = kDefaultScrollLinesPerWheelDelta;
+        SystemParametersInfo(SPI_GETWHEELSCROLLLINES, 0, &scroll_lines, 0);
+        dy *= scroll_lines;
+
+        static const float kScrollbarPixelsPerLine = 100.0f / 3.0f;
+        dy *= kScrollbarPixelsPerLine;
 
         app_window.onScroll(0, dy);
         return 0;
     }
 
     case WM_MOUSEHWHEEL: {
-        float dx = static_cast<float>(GET_WHEEL_DELTA_WPARAM(wParam));
+        float dx = static_cast<float>(GET_WHEEL_DELTA_WPARAM(wParam)) / WHEEL_DELTA;
+
+        static const unsigned long kDefaultScrollCharsPerWheelDelta = 1;
+        unsigned long scroll_chars = kDefaultScrollCharsPerWheelDelta;
+        SystemParametersInfo(SPI_GETWHEELSCROLLCHARS, 0, &scroll_chars, 0);
+        dx *= scroll_chars;
+
+        static const float kScrollbarPixelsPerLine = 100.0f / 3.0f;
+        dx *= kScrollbarPixelsPerLine;
 
         app_window.onScroll(dx, 0);
         return 0;

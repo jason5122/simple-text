4aa84a6216ac8aed1432e6f994202f0349975c87
Jason Han <jjhan@alum.vassar.edu>
2024-04-29 16:23:39 -0400

    Add default color scheme fallback

--- config/color_scheme.cc
+++ config/color_scheme.cc
@@ -2,6 +2,7 @@
 #include "base/rgb.h"
 #include "color_scheme.h"
 #include "glaze/glaze.hpp"
+#include "glaze/json/write.hpp"
 #include <iostream>
 
 namespace config {
@@ -15,19 +16,47 @@ struct Schema {
     std::string scroll_bar;
 };
 
+static const Schema kDefaultLightSchema{
+    .foreground = "#333333",
+    .background = "#fdfdfd",
+    .caret = "#5fb4b4",
+    .tab_bar = "#bebebe",
+    .side_bar = "#ebedef",
+    .status_bar = "#c7cbd1",
+    .scroll_bar = "#b6b6b6",
+};
+
+static const Schema kDefaultDarkSchema{
+    .foreground = "#d8dee9",
+    .background = "#303841",
+    .caret = "#f9ae58",
+    .tab_bar = "#4f565e",
+    .side_bar = "#22262a",
+    .status_bar = "#2e3238",
+    .scroll_bar = "#6a7076",
+};
+
 ColorScheme::ColorScheme() {
     fs::path color_scheme_path = DataPath() / "color_scheme_light.json";
     // fs::path color_scheme_path = DataPath() / "color_scheme_dark.json";
 
-    glz::parse_error error;
-
     Schema schema;
-    std::string buffer;
 
-    // TODO: Handle errors in a better way.
-    error = glz::read_file_json(schema, color_scheme_path.string(), buffer);
-    if (error) {
-        std::cerr << glz::format_error(error, buffer) << '\n';
+    std::string buffer;
+    if (fs::exists(color_scheme_path)) {
+        // TODO: Handle errors in a better way.
+        glz::parse_error error = glz::read_file_json(schema, color_scheme_path.string(), buffer);
+        if (error) {
+            std::cerr << glz::format_error(error, buffer) << '\n';
+        }
+    } else {
+        schema = kDefaultLightSchema;
+        // schema = kDefaultDarkSchema;
+
+        glz::write_error error = glz::write_file_json(schema, color_scheme_path.string(), buffer);
+        if (error) {
+            std::cerr << "Could not write color scheme to " << color_scheme_path << ".\n";
+        }
     }
 
     // TODO: Is there a better way to do this?

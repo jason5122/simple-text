e83cc8b09edf7f449b7e89a662815a864149f6f0
Jason Han <jjhan@alum.vassar.edu>
2024-05-09 05:46:38 -0400

    Allow //util/profile_util.h to accept milliseconds

--- renderer/tests/hash_map_test.cc
+++ renderer/tests/hash_map_test.cc
@@ -1,4 +1,5 @@
 #include "absl/container/flat_hash_map.h"
+#include "util/profile_util.h"
 #include "gtest/gtest.h"
 #include <random>
 #include <unordered_map>
@@ -11,36 +12,35 @@ struct Dummy {
     int z;
 };
 
-TEST(HashMapTest, STL) {
+TEST(HashMapTest, StringKey) {
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist6(1, kIterations);
 
-    std::unordered_map<std::string, Dummy> cache;
-    for (size_t i = 0; i < kIterations; i++) {
-        std::string key = std::to_string(dist6(rng));
-        Dummy dummy{
-            .x = 1,
-            .y = 2,
-            .z = 3,
-        };
-        cache.insert({std::move(key), std::move(dummy)});
+    {
+        PROFILE_BLOCK_WITH_DURATION("StringKey: STL", std::chrono::milliseconds);
+        std::unordered_map<std::string, Dummy> cache_stl;
+        for (size_t i = 0; i < kIterations; i++) {
+            std::string key = std::to_string(dist6(rng));
+            Dummy dummy{
+                .x = 1,
+                .y = 2,
+                .z = 3,
+            };
+            cache_stl.insert({std::move(key), std::move(dummy)});
+        }
     }
-}
-
-TEST(HashMapTest, Abseil) {
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(1, kIterations);
-
-    absl::flat_hash_map<std::string, Dummy> cache;
-    for (size_t i = 0; i < kIterations; i++) {
-        std::string key = std::to_string(dist6(rng));
-        Dummy dummy{
-            .x = 1,
-            .y = 2,
-            .z = 3,
-        };
-        cache.insert({std::move(key), std::move(dummy)});
+    {
+        PROFILE_BLOCK_WITH_DURATION("StringKey: Abseil", std::chrono::milliseconds);
+        absl::flat_hash_map<std::string, Dummy> cache_absl;
+        for (size_t i = 0; i < kIterations; i++) {
+            std::string key = std::to_string(dist6(rng));
+            Dummy dummy{
+                .x = 1,
+                .y = 2,
+                .z = 3,
+            };
+            cache_absl.insert({std::move(key), std::move(dummy)});
+        }
     }
 }
--- util/profile_util.h
+++ util/profile_util.h
@@ -1,18 +1,37 @@
 #pragma once
 
+#include "util/not_copyable_or_movable.h"
 #include <chrono>
 #include <iostream>
 
 // https://stackoverflow.com/a/37607676
-struct profiler {
+template <typename Duration = std::chrono::microseconds> class Profiler {
+public:
+    NOT_COPYABLE(Profiler)
+    NOT_MOVABLE(Profiler)
+    Profiler(std::string const& n) : name(n), t1(std::chrono::high_resolution_clock::now()) {}
+    ~Profiler() {
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<Duration>(t2 - t1).count();
+
+        std::string unit = "units";
+        if (std::is_same<Duration, std::chrono::microseconds>::value) {
+            unit = "µs";
+        } else if (std::is_same<Duration, std::chrono::milliseconds>::value) {
+            unit = "ms";
+        }
+        std::cerr << name << ": " << duration << ' ' << unit << '\n';
+    }
+
+private:
     std::string name;
     std::chrono::high_resolution_clock::time_point t1;
-    profiler(std::string const& n) : name(n), t1(std::chrono::high_resolution_clock::now()) {}
-    ~profiler() {
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-        std::cerr << name << ": " << duration << " µs\n";
-    }
 };
 
-#define PROFILE_BLOCK(pbn) profiler _pfinstance(pbn)
+#define PROFILE_BLOCK(name) Profiler _pfinstance(name)
+#define PROFILE_BLOCK_WITH_DURATION(name, duration) Profiler<duration> _pfinstance(name)
+
+// template <typename Duration = std::chrono::microseconds>
+// inline Profiler<Duration> ProfileBlock(std::string name) {
+//     return Profiler<Duration>(name);
+// }

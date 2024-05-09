f069614b53cc6459cbb6e64b569e05733b3acdf5
Jason Han <jjhan@alum.vassar.edu>
2024-05-09 06:05:06 -0400

    Add //third_party/unordered_dense/

--- renderer/tests/hash_map_test.cc
+++ renderer/tests/hash_map_test.cc
@@ -1,4 +1,5 @@
 #include "absl/container/flat_hash_map.h"
+#include "third_party/unordered_dense/ankerl/unordered_dense.h"
 #include "util/profile_util.h"
 #include "gtest/gtest.h"
 #include <random>
@@ -30,9 +31,22 @@ TEST(HashMapTest, StringKey) {
             cache_stl.insert({std::move(key), std::move(dummy)});
         }
     }
+    // {
+    //     PROFILE_BLOCK_WITH_DURATION("StringKey: Abseil", std::chrono::milliseconds);
+    //     absl::flat_hash_map<std::string, Dummy> cache_absl;
+    //     for (size_t i = 0; i < kIterations; i++) {
+    //         std::string key = std::to_string(dist6(rng));
+    //         Dummy dummy{
+    //             .x = 1,
+    //             .y = 2,
+    //             .z = 3,
+    //         };
+    //         cache_absl.insert({std::move(key), std::move(dummy)});
+    //     }
+    // }
     {
-        PROFILE_BLOCK_WITH_DURATION("StringKey: Abseil", std::chrono::milliseconds);
-        absl::flat_hash_map<std::string, Dummy> cache_absl;
+        PROFILE_BLOCK_WITH_DURATION("StringKey: Ankerl", std::chrono::milliseconds);
+        ankerl::unordered_dense::map<std::string, Dummy> cache_ankerl;
         for (size_t i = 0; i < kIterations; i++) {
             std::string key = std::to_string(dist6(rng));
             Dummy dummy{
@@ -40,7 +54,53 @@ TEST(HashMapTest, StringKey) {
                 .y = 2,
                 .z = 3,
             };
-            cache_absl.insert({std::move(key), std::move(dummy)});
+            cache_ankerl.insert({std::move(key), std::move(dummy)});
+        }
+    }
+}
+
+TEST(HashMapTest, IntegerKey) {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(1, kIterations);
+
+    {
+        PROFILE_BLOCK_WITH_DURATION("IntegerKey: STL", std::chrono::milliseconds);
+        std::unordered_map<uint32_t, Dummy> cache_stl;
+        for (size_t i = 0; i < kIterations; i++) {
+            uint32_t key = dist6(rng);
+            Dummy dummy{
+                .x = 1,
+                .y = 2,
+                .z = 3,
+            };
+            cache_stl.insert({std::move(key), std::move(dummy)});
+        }
+    }
+    // {
+    //     PROFILE_BLOCK_WITH_DURATION("IntegerKey: Abseil", std::chrono::milliseconds);
+    //     absl::flat_hash_map<uint32_t, Dummy> cache_absl;
+    //     for (size_t i = 0; i < kIterations; i++) {
+    //         uint32_t key = dist6(rng);
+    //         Dummy dummy{
+    //             .x = 1,
+    //             .y = 2,
+    //             .z = 3,
+    //         };
+    //         cache_absl.insert({std::move(key), std::move(dummy)});
+    //     }
+    // }
+    {
+        PROFILE_BLOCK_WITH_DURATION("IntegerKey: Ankerl", std::chrono::milliseconds);
+        ankerl::unordered_dense::map<uint32_t, Dummy> cache_ankerl;
+        for (size_t i = 0; i < kIterations; i++) {
+            uint32_t key = dist6(rng);
+            Dummy dummy{
+                .x = 1,
+                .y = 2,
+                .z = 3,
+            };
+            cache_ankerl.insert({std::move(key), std::move(dummy)});
         }
     }
 }

1ac7cf3a1c2f981c82e8e48e56f05c2267eef407
Jason Han <jjhan@alum.vassar.edu>
2024-05-10 13:44:02 -0400

    Use reinterpret_cast for void**

--- font/directwrite_rasterizer.cc
+++ font/directwrite_rasterizer.cc
@@ -42,7 +42,7 @@ bool FontRasterizer::setup(int id, std::string main_font_name, int font_size) {
     this->id = id;
 
     DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory2),
-                        static_cast<IUnknown**>(&pimpl->dwrite_factory));
+                        reinterpret_cast<IUnknown**>(&pimpl->dwrite_factory));
 
     IDWriteFontCollection* font_collection;
     pimpl->dwrite_factory->GetSystemFontCollection(&font_collection);

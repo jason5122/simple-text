2b8cc830ffc3701eb635999b1ea74e9c2495beb9
Jason Han <jjhan@alum.vassar.edu>
2024-06-06 08:57:29 -0400

    Optimize to the max

--- base/filesystem/file_reader.cc
+++
@@ -1,8 +0,0 @@
-#include "file_reader.h"
-#include <fstream>
-
-std::string ReadFile(fs::path file_name) {
-    std::ifstream in(file_name);
-    std::string contents((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
-    return contents;
-}

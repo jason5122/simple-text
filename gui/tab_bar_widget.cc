5fdcdd374bfa2ad01fd542e61c3c931b004090a0
Jason Han <jjhan@alum.vassar.edu>
2024-06-27 10:02:30 -0400

    Refine tab bar widget more

--- gui/tab_bar_widget.cc
+++ gui/tab_bar_widget.cc
@@ -22,8 +22,8 @@ void TabBarWidget::draw() {
     int tab_width = 360;
 
     // TODO: Unify `RectRenderer::addTab()` with `RectRenderer::addRect()`.
-    rect_renderer.addTab(position + padding_top, {tab_width, tab_height}, {253, 253, 253, 255},
-                         tab_corner_radius);
+    // rect_renderer.addTab(position + padding_top, {tab_width, tab_height}, {100, 100, 100, 255},
+    //                      tab_corner_radius);
 
     renderer::Size image_size =
         image_renderer.getImageSize(renderer::ImageRenderer::kPanelCloseImageIndex);
@@ -33,10 +33,22 @@ void TabBarWidget::draw() {
     image_pos.y += tab_height / 2;
     image_pos.y -= image_size.height / 2;
 
-    image_pos.x += tab_width - image_size.width - tab_corner_radius * 2;
+    image_pos.x += 340 - image_size.width;
     image_pos.x -= 15;  // TODO: Don't hard code this value.
-    image_renderer.addImage(renderer::ImageRenderer::kPanelCloseImageIndex, image_pos,
-                            {158, 158, 158, 255});
+
+    for (size_t i = 0; i < 3; i++) {
+        image_renderer.addImage(renderer::ImageRenderer::kPanelCloseImageIndex, image_pos,
+                                {158, 158, 158, 255});
+        image_pos += renderer::Point{340, 0};
+    }
+
+    int tab_index = 2;
+    rect_renderer.addTab(position + padding_top + renderer::Point{340 * tab_index, 0},
+                         {tab_width, tab_height}, {253, 253, 253, 255}, tab_corner_radius);
+
+    // TODO: Figure out why we need to add 1 to `26 / 2`.
+    rect_renderer.addRect(position + renderer::Point{350 * 1 - 2, 26 / 2 + 1},
+                          {2, size.height - 26}, {148, 149, 149, 255});
 }
 
 }

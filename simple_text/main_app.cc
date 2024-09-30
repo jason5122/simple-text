fee4bd4cf353ee4964dbe77034507a8deb413748
Jason Han <jason.han@columbia.edu>
2024-09-29 20:13:23 -0400

    Add initial Rust support

--- simple_text/main_app.cc
+++ simple_text/main_app.cc
@@ -1,6 +1,10 @@
 #include "simple_text/editor_app.h"
 
+extern "C" void hello_from_rust();
+
 int SimpleTextMain(int argc, char* argv[]) {
+    hello_from_rust();
+
     EditorApp editor_app;
     editor_app.run();
     return 0;

916a077baad641b22bcc39567c76cfc6fa02744c
Jason Han <jason.han@columbia.edu>
2024-11-12 21:01:56 -0500

    Implement SyntaxHighlighter::startQuery() for more efficient highlight querying

--- syntax_highlighter/syntax_highlighter.h
+++ syntax_highlighter/syntax_highlighter.h
@@ -9,6 +9,20 @@
 
 namespace highlight {
 
+class QueryCursor {
+public:
+    ~QueryCursor();
+
+    bool nextMatch(TSPoint& start, TSPoint& end, uint32_t& capture_index);
+
+private:
+    friend class SyntaxHighlighter;
+
+    QueryCursor(size_t start_line, size_t end_line, TSTree* tree, TSQuery* query);
+
+    TSQueryCursor* cursor;
+};
+
 class SyntaxHighlighter {
 public:
     SyntaxHighlighter();
@@ -19,6 +33,7 @@ public:
     void edit(size_t start_byte, size_t old_end_byte, size_t new_end_byte);
 
     std::vector<Highlight> getHighlights(size_t start_line, size_t end_line) const;
+    QueryCursor startQuery(size_t start_line, size_t end_line) const;
     const Rgb& getColor(size_t capture_index) const;
 
 private:

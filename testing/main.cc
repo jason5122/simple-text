b9b792f78d38dbf0c004ad9b874aadff0ce78ee2
Jason Han <jason.han@columbia.edu>
2026-08-03 03:20:42 -0400

    Add fuzztest coverage flag

--- testing/main.cc
+++ testing/main.cc
@@ -1,16 +1,8 @@
-#include <absl/debugging/failure_signal_handler.h>
-#include <absl/debugging/symbolize.h>
 #include <fuzztest/init_fuzztest.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
-    absl::InitializeSymbolizer(argv[0]);
-    absl::FailureSignalHandlerOptions options;
-    options.call_previous_handler = true;
-    absl::InstallFailureSignalHandler(options);
-
     testing::InitGoogleTest(&argc, argv);
-
     fuzztest::ParseAbslFlags(argc, argv);
     fuzztest::InitFuzzTest(&argc, &argv);
     return RUN_ALL_TESTS();
